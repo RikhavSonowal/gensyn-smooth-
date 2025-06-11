137206e9218a9a07b347560c6f354dbfe26a905f
Rikhav Sonowal <120777605+RikhavSonowal@users.noreply.github.com>
2025-06-11 08:50:13 +0530

    Update page.tsx

--- page.tsx
+++ page.tsx
@@ -1,23 +1,127 @@
-'use client'
-import { useEffect } from 'react'
-import { useSearchParams } from 'next/navigation'
+"use client";
+import {
+  useAuthModal,
+  useLogout,
+  useSigner,
+  useSignerStatus,
+  useUser,
+} from "@account-kit/react";
+import { useCallback, useEffect, useState } from "react";
 
-export default function Page() {
-  const searchParams = useSearchParams()
-  const org_id = searchParams.get('org_id')
-  const hf_token = searchParams.get('hf_token')
+export default function Home() {
+  const user = useUser();
+  const { openAuthModal } = useAuthModal();
+  const signerStatus = useSignerStatus();
+  const { logout } = useLogout();
+  const signer = useSigner();
+
+  const [createdApiKey, setCreatedApiKey] = useState(false);
+  const [sawDisconnected, setSawDisconnected] = useState(false);
+  const [sawConnected, setSawConnected] = useState(false);
+
+  // For some reason, the signer status jumps from disconnected to initializing, 
+  // which makes keeping track of the status here tricky.
+  // Record that we ever saw a disconnected or connected status and make decisions on that.
+  useEffect(() => {
+    if (signerStatus.status === "DISCONNECTED") {
+      setSawDisconnected(true);
+    }
+    if (signerStatus.status === "CONNECTED") {
+      setSawConnected(true);
+    }
+  }, [signerStatus.status]);
+
+  const handleAll = useCallback(async () => {
+    if (!user || !signer) {
+      console.log("handleAll: no user or signer");
+      return;
+    }
+
+    try {
+      const whoamiStamp = await signer?.inner.stampWhoami();
+      if (!whoamiStamp) {
+        console.log("No whoami stamp");
+        return;
+      }
+
+      const resp = await fetch("/api/get-api-key", {
+        method: "POST",
+        body: JSON.stringify({ whoamiStamp }),
+      });
+
+      const { publicKey } = await resp.json() || {};
+      if (!publicKey) {
+        console.log("No public key");
+        return;
+      }
+
+      await signer?.inner.experimental_createApiKey({
+        name: `server-signer-${new Date().getTime()}`,
+        publicKey,
+        expirationSec: 60 * 60 * 24 * 62, // 62 days
+      });
+
+      await fetch("/api/set-api-key-activated", {
+        method: "POST",
+        body: JSON.stringify({ orgId: user.orgId, apiKey: publicKey }),
+      });
+
+      setCreatedApiKey(true);
+    } catch (err) {
+      console.error(err);
+      window.alert("Error logging in. See console for details.");
+    }
+  }, [signer, user]);
 
   useEffect(() => {
-    const isOrgIdValid = org_id && org_id.length === 36
-    const isHfTokenValid = hf_token && hf_token.length > 10
-    if (isOrgIdValid && isHfTokenValid) {
-      localStorage.setItem('modal_org_id', org_id as string)
-      localStorage.setItem('hf_token', hf_token as string)
-      window.location.href = '/swarm'
-    } else {
-      window.location.href = 'https://modal.com/org/gensyn-ai/spaces'
+    if (sawConnected) {
+      handleAll();
     }
-  }, [org_id, hf_token])
+  }, [handleAll, sawConnected]);
 
-  return null
+
+  // Show alert if crypto.subtle isn't available.
+  useEffect(() => {
+    if (typeof window === undefined) {
+      return;
+    }
+    try {
+      if (typeof window.crypto.subtle !== "object") {
+        throw new Error("window.crypto.subtle is not available");
+      }
+    } catch (err) {
+      alert(
+        "Crypto api is not available in browser. Please be sure that the app is being accessed via localhost or a secure connection.",
+      );
+    }
+  }, []);
+
+  return (
+    <main className="flex min-h-screen flex-col items-center gap-4 justify-center text-center">
+      {(!sawDisconnected && !sawConnected) || (user && !createdApiKey) ? (
+        <>Loading...</>
+      ) : user ? (
+        <div className="card">
+          <div className="flex flex-col gap-2 p-2">
+            <p className="text-xl font-bold">
+              YOU ARE SUCCESSFULLY LOGGED IN TO THE GENSYN TESTNET
+            </p>
+            <button className="btn btn-primary mt-6" onClick={() => logout()}>
+              Log out
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div className="card">
+          <p className="text-xl font-bold">LOGIN TO THE GENSYN TESTNET</p>
+          <div className="flex flex-col gap-2 p-2">
+            <button className="btn btn-primary mt-6" onClick={openAuthModal}>
+              Login
+            </button>
+          </div>
+        </div>
+      )}
+    </main>
+  );
 }
+    
